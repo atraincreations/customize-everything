5c8dac57132db806fff74a780814b6422566198b
Alex Underwood <underwoodapu@gmail.com>
2021-11-26 23:38:35 -0800

    quick update
    column width adjustment

--- change-text/src/Firm.js
+++ change-text/src/Firm.js
@@ -15,22 +15,20 @@ class Firm extends Component {
                 <div className='firm-title'>
                     <h1>Firm Name</h1>
                 </div>
-                <table className='grid-container'>
+                <div className='grid-container'>
                     <h4>Firm Name:</h4>
                     <p>firm name here</p>
-                    <h4>Firm Name:</h4>
-                    <p>firm name here</p>
-                    <h4>Firm Name:</h4>
-                    <p>firm name here</p>
-                    <h4>Firm Name:</h4>
-                    <p>firm name here</p>
-                    <h4>Firm Name:</h4>
-                    <p>firm name here</p>
-                    <h4>Firm Name:</h4>
-                    <p>firm name here</p>
-                    <h4>Firm Name:</h4>
-                    <p>firm name here</p>
-                </table>
+                    <h4>Address:</h4>
+                    <p>address here</p>
+                    <h4>Phone Number:</h4>
+                    <p>phone number here</p>
+                    <h4>Disclaimer:</h4>
+                    <p>disclaimer here</p>
+                    <h4>Advisor Count:</h4>
+                    <p>count here</p>
+                    <h4>Admin Count:</h4>
+                    <p>count here</p>
+                </div>
                 
             </div>
         );
